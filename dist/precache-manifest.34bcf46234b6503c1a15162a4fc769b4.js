ec53d0ccd3fd595813ab9db87dff94212b355408
Ilana Mostovski <ilana.mostovski@gmail.com>
2018-05-16 23:06:38 +0300

    dist folder added

---
+++ dist/precache-manifest.34bcf46234b6503c1a15162a4fc769b4.js
@@ -0,0 +1,446 @@
+self.__precacheManifest = [
+  {
+    "revision": "08a2f57ad9790ccef0d5cdfe439860be",
+    "url": "/img/dogs/julia_1.jpeg"
+  },
+  {
+    "revision": "9e909691f40f0b58bf97c5a2e5a0df60",
+    "url": "/users and dogs.xlsx"
+  },
+  {
+    "revision": "d514eced9eb128b1c35d51d7577fe0e5",
+    "url": "/img/dogs/julia_3.jpeg"
+  },
+  {
+    "revision": "8e2b7899176f0f6f3972",
+    "url": "/js/vendor.8e2b7899.js"
+  },
+  {
+    "revision": "87092352e380013ca6dbd0dbd2f6ba38",
+    "url": "/img/dogs/julia_4.jpeg"
+  },
+  {
+    "revision": "a567bcb3145a3d4caaac2859edae0a03",
+    "url": "/img/dog-treats.a567bcb3.png"
+  },
+  {
+    "revision": "8b7c08e9f2722591fe94b4c1d484c1db",
+    "url": "/img/logo/paw.jpg"
+  },
+  {
+    "revision": "1414d97352d78a1bb1664c22f0460162",
+    "url": "/img/logo/paw.png"
+  },
+  {
+    "revision": "e4356e2fcd732745023477385dced99b",
+    "url": "/img/dogs/julia_2.jpeg"
+  },
+  {
+    "revision": "0bf6b28bdd2404959020935b063d8ecf",
+    "url": "/img/logo/logo.xcf"
+  },
+  {
+    "revision": "53151e90b99e0cf6698e0f63ff7e65bf",
+    "url": "/img/logo/bone.png"
+  },
+  {
+    "revision": "fba4803a043d4e34090235fd1c260968",
+    "url": "/img/logo/example.jpg"
+  },
+  {
+    "revision": "65ad4cb043f14f04588d86aac6fcce69",
+    "url": "/img/logo/bone.html"
+  },
+  {
+    "revision": "51dc494d418c88f4966f7f4282689bca",
+    "url": "/img/dogs/toy_6.jpeg"
+  },
+  {
+    "revision": "a47d24e3e392abd0450cd53a6bd3bf11",
+    "url": "/img/dogs/toy_5.jpeg"
+  },
+  {
+    "revision": "732f6aadbe19bfe9751dce97a5ec7f59",
+    "url": "/img/dogs/toy_4.jpeg"
+  },
+  {
+    "revision": "8974450d4e9eb4660f9b70ff096ba5ba",
+    "url": "/img/logo.8974450d.png"
+  },
+  {
+    "revision": "d67d841a5765707fb1b37b24177b11e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e9fc1bcdbc893cbab0d6252e6ee1e23",
+    "url": "/img/dogs/toy_2.jpeg"
+  },
+  {
+    "revision": "4eaed585c0fd07aef896352f6fca1f3e",
+    "url": "/img/dogs/toy_3.jpeg"
+  },
+  {
+    "revision": "3637bb12aab99c6f51e48c90858e3e97",
+    "url": "/img/dogs/toy_1.jpeg"
+  },
+  {
+    "revision": "8974450d4e9eb4660f9b70ff096ba5ba",
+    "url": "/img/logo/logo.png"
+  },
+  {
+    "revision": "7b64788602aef4a8e884ba2a81545dbe",
+    "url": "/img/dogs/tim_1.jpeg"
+  },
+  {
+    "revision": "a563d6a0c6c38c234aad9f53e17735a0",
+    "url": "/img/dogs/stella_1.jpeg"
+  },
+  {
+    "revision": "da83132f9d06ce7ada81ebb1f27b889d",
+    "url": "/img/dogs/thomas_1.jpeg"
+  },
+  {
+    "revision": "0ad02d7e5ca0f957828642e41d1cae48",
+    "url": "/img/dogs/bagel_1.jpeg"
+  },
+  {
+    "revision": "177f51fc11df47fd2732613e2bee054d",
+    "url": "/img/dogs/steev_2.jpeg"
+  },
+  {
+    "revision": "c8b42aa1700b82f6cf951c7a51a9e313",
+    "url": "/img/dogs/anton_1.jpeg"
+  },
+  {
+    "revision": "12174ffcd63424884bc95275eb9cc16a",
+    "url": "/img/dogs/bella_2.jpeg"
+  },
+  {
+    "revision": "10b378822fcd852f91eb0c91e7659f8e",
+    "url": "/img/dogs/bella_1.jpeg"
+  },
+  {
+    "revision": "1559d0c883c3755e2637603493823887",
+    "url": "/img/dogs/bella_4.jpeg"
+  },
+  {
+    "revision": "a2815890dcf96f510ea9ebf705526363",
+    "url": "/img/dogs/bella_3.jpeg"
+  },
+  {
+    "revision": "2edeebd5bd3ab966e0d786e108d63157",
+    "url": "/img/dogs/bob_1.jpeg"
+  },
+  {
+    "revision": "1eda6159900c4db9c968976abd4779fc",
+    "url": "/img/dogs/bob_2.jpeg"
+  },
+  {
+    "revision": "51160f7bf20eb65f69dd9b2bfd65fa0b",
+    "url": "/img/dogs/burger_2.jpeg"
+  },
+  {
+    "revision": "b9e02a4a5bb8590c047ea80ec701dd2a",
+    "url": "/img/dogs/burger_1.jpeg"
+  },
+  {
+    "revision": "3f376cd8b55ad1438e1df61cc3d231d0",
+    "url": "/img/dogs/candy_1.jpeg"
+  },
+  {
+    "revision": "2a05a5c18bd3f05e99b8f4300fc3a2f6",
+    "url": "/img/dogs/burger_3.jpeg"
+  },
+  {
+    "revision": "c936e70d2e10925505796eac3ff1b66a",
+    "url": "/img/dogs/candy_2.jpeg"
+  },
+  {
+    "revision": "2672f9972bc37be6dae842a49b6bdfab",
+    "url": "/img/dogs/candy_4.jpeg"
+  },
+  {
+    "revision": "efaaa03ee62e3f6d9d88adf1adcb1c1f",
+    "url": "/img/dogs/candy_3.jpeg"
+  },
+  {
+    "revision": "625d21d081e295d6b457233087e9f57b",
+    "url": "/img/dogs/charly_2.jpeg"
+  },
+  {
+    "revision": "a24070aa706c43d08be761bb66a6dee8",
+    "url": "/img/dogs/charly_1.jpeg"
+  },
+  {
+    "revision": "896ec8e0a6641db1e6c23862096069f5",
+    "url": "/img/dogs/charly_3.jpeg"
+  },
+  {
+    "revision": "91e18b31cb1d3ee5f0d3a2ac4cdb2b22",
+    "url": "/img/dogs/cooper_1.jpg"
+  },
+  {
+    "revision": "9dc40fdf3eac4b44a907f4269d854ed0",
+    "url": "/img/dogs/damian_1.jpeg"
+  },
+  {
+    "revision": "00848729d9b8fcee614923cbdd7c38ad",
+    "url": "/img/dogs/dian_1.jpg"
+  },
+  {
+    "revision": "497eed6945a8a8ac647dfb197a395f08",
+    "url": "/img/dogs/donna_1.jpeg"
+  },
+  {
+    "revision": "f330e2abdca2c4c24934fe636d62eaf4",
+    "url": "/img/dogs/donna_2.jpeg"
+  },
+  {
+    "revision": "19e77d8bdb35a8bb8c99690079b96e3d",
+    "url": "/img/dogs/fred_1.jpeg"
+  },
+  {
+    "revision": "c218ae7dd315c469a8021f9c8d2b7b83",
+    "url": "/img/dogs/foxy_1.jpeg"
+  },
+  {
+    "revision": "5e3aa88f26a0ea4e1682e12d5b1ef5e6",
+    "url": "/img/dogs/hamlet_1.jpeg"
+  },
+  {
+    "revision": "2773654dcc461020fd399ff89833d0b4",
+    "url": "/img/dogs/jonny_1.jpeg"
+  },
+  {
+    "revision": "f3a4134e87c5d1099514552a32300804",
+    "url": "/img/dogs/hero_1.jpeg"
+  },
+  {
+    "revision": "b04634a9f456ceb3d7d4e2fbd03c2e74",
+    "url": "/img/dogs/jonny_2.jpeg"
+  },
+  {
+    "revision": "9b6cf14873794cc5a841",
+    "url": "/js/app.9b6cf148.js"
+  },
+  {
+    "revision": "bb41d6d80c1fdaad6874",
+    "url": "/js/manifest.bb41d6d8.js"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/img/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "c043ef55ccc4ad9c652277413b74521a",
+    "url": "/img/dogs/karen_1.jpeg"
+  },
+  {
+    "revision": "a3cff44312d1f892e37667c92322531a",
+    "url": "/img/dogs/hamlet_2.jpeg"
+  },
+  {
+    "revision": "c395e3966955bb0b988802f756fcaea4",
+    "url": "/img/dogs/Kimi_1.jpeg"
+  },
+  {
+    "revision": "6f642a750fbc73949008856cfe1db0eb",
+    "url": "/img/dogs/leo_1.jpeg"
+  },
+  {
+    "revision": "7f333093edb584f785f872db7332b825",
+    "url": "/img/dogs/kira_1.jpeg"
+  },
+  {
+    "revision": "7dee4443dc806634d6b2473d127f711a",
+    "url": "/img/dogs/linda_1.jpeg"
+  },
+  {
+    "revision": "754b9cab4531010956103fb4ec502527",
+    "url": "/img/dogs/joy_1.jpeg"
+  },
+  {
+    "revision": "082c0fe0ea6f7e9b967681ab6dec265c",
+    "url": "/img/dogs/loren_1.jpg"
+  },
+  {
+    "revision": "205055cae41528e1b73d9d2c7820b272",
+    "url": "/img/dogs/lisa_1.jpeg"
+  },
+  {
+    "revision": "6baf913416ad1051151c0f094e13540b",
+    "url": "/img/dogs/luna_1.jpeg"
+  },
+  {
+    "revision": "d7ec9342e6a9c191e064802219656d47",
+    "url": "/img/dogs/luna_3.jpeg"
+  },
+  {
+    "revision": "7589d6a947bec29bacf94a6c5f973b8e",
+    "url": "/img/dogs/luna_2.jpeg"
+  },
+  {
+    "revision": "a5e62f53f7278da372dc8c3e10a44fa2",
+    "url": "/img/dogs/luyi_1.jpg"
+  },
+  {
+    "revision": "b802c1242ce1901c105ea4f3d1f6d315",
+    "url": "/img/dogs/margaret_1.jpeg"
+  },
+  {
+    "revision": "857b6816daffcc7f9bf8622ea97bf687",
+    "url": "/img/dogs/martin_1.jpeg"
+  },
+  {
+    "revision": "2274ebec141f23b9479b2a8f26dcdb18",
+    "url": "/img/dogs/marshmelo_1.jpeg"
+  },
+  {
+    "revision": "14cddedf6a06ef6e47329985f99f5214",
+    "url": "/img/dogs/max_2.jpeg"
+  },
+  {
+    "revision": "89f44bfd4c8e7fafe7023df0574f4759",
+    "url": "/img/dogs/max_1.jpeg"
+  },
+  {
+    "revision": "9d550c561b52e0823ee1e20435773a89",
+    "url": "/img/dogs/melisa_1.jpeg"
+  },
+  {
+    "revision": "1b023177a7decb81899ae487725b6d90",
+    "url": "/img/dogs/mia_1.jpeg"
+  },
+  {
+    "revision": "3d47bc93a3be740624347c099ca34d40",
+    "url": "/img/dogs/martin_2.jpeg"
+  },
+  {
+    "revision": "80814b7ea386393da0006bf56d2a3759",
+    "url": "/img/dogs/nick_1.jpeg"
+  },
+  {
+    "revision": "ad361a181400146b4b5bbef9d50128ec",
+    "url": "/img/dogs/mila_1.jpeg"
+  },
+  {
+    "revision": "0eba699bf9cd5af8bed6af552762f376",
+    "url": "/img/dogs/rita_1.jpeg"
+  },
+  {
+    "revision": "01e10d8418f53d89fe8257e0d26aa06f",
+    "url": "/img/dogs/robert_1.jpeg"
+  },
+  {
+    "revision": "8b3876ef13fceec8f50dbe5ba1e32f0b",
+    "url": "/img/dogs/rocky_2.jpeg"
+  },
+  {
+    "revision": "7ad1a930dab3c099939b66267b5c57f8",
+    "url": "/img/dogs/rocky_1.jpeg"
+  },
+  {
+    "revision": "e76ea280a794b5249bcccc5ea8a0751d",
+    "url": "/img/dogs/snow_1.jpeg"
+  },
+  {
+    "revision": "f9b73aaff9b6b533b8f80f0ba7fa28fc",
+    "url": "/img/dogs/shila_1.jpeg"
+  },
+  {
+    "revision": "b2f0840ad70375b115a58d29e129d03f",
+    "url": "/img/dogs/snow_2.jpeg"
+  },
+  {
+    "revision": "13c85d47eec9a16c7bb4d76948af7281",
+    "url": "/img/dogs/snow_3.jpeg"
+  },
+  {
+    "revision": "d986352af0730661afe3072004a66e6f",
+    "url": "/img/dogs/snow_4.jpeg"
+  },
+  {
+    "revision": "24ee24480342572401180405e1d44e8d",
+    "url": "/img/dogs/star_2.jpeg"
+  },
+  {
+    "revision": "1a9d5c1821f65e28ec9bda1e9051b1ea",
+    "url": "/img/dogs/steev_1.jpeg"
+  },
+  {
+    "revision": "223d406cc7b6bab98a2095d305e76146",
+    "url": "/img/dogs/star_1.jpeg"
+  },
+  {
+    "revision": "ca47969fcf6e949d6ff7366d0b7a75a6",
+    "url": "/fonts/CutiePatootie.ca47969f.ttf"
+  },
+  {
+    "revision": "3fa4654e364e7afbebde67c7fa346343",
+    "url": "/img/bgrd/shattered-island.gif"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "c12a645e1891c2e0aa9511cf5122dcd6",
+    "url": "/img/bgrd/dog-background.jpg"
+  },
+  {
+    "revision": "29c8709811946a854d5c7b463aa74dc6",
+    "url": "/img/bgrd/paw.jpg"
+  },
+  {
+    "revision": "a567bcb3145a3d4caaac2859edae0a03",
+    "url": "/img/bgrd/dog-treats.png"
+  },
+  {
+    "revision": "5576a8ed5dab81546997701927949d6f",
+    "url": "/img/bgrd/dog-bone.jpg"
+  },
+  {
+    "revision": "9871420118879979d4b2a8107ce7c680",
+    "url": "/fonts/SweetHipster.98714201.ttf"
+  },
+  {
+    "revision": "0436a9ab4c9504311cec70dd650944cb",
+    "url": "/img/bg1.0436a9ab.jpg"
+  },
+  {
+    "revision": "0436a9ab4c9504311cec70dd650944cb",
+    "url": "/img/bgrd/bg1.jpg"
+  },
+  {
+    "revision": "4097c0daeadbfa413ea503a3b6ae524e",
+    "url": "/img/bgrd/bg.jpg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "90a308229f73a530e42b5063ec67bb24",
+    "url": "/fonts/RainbowBridge.90a30822.ttf"
+  },
+  {
+    "revision": "8a5d150d70ddee86408dd6a2c693b5c9",
+    "url": "/fonts/Digitalt.8a5d150d.ttf"
+  },
+  {
+    "revision": "2e4f96fb75fd98798f3d9951507c72a5",
+    "url": "/fonts/BiggerLove.2e4f96fb.ttf"
+  },
+  {
+    "revision": "9b6cf14873794cc5a841",
+    "url": "/css/app.29cf32be.css"
+  }
+];
